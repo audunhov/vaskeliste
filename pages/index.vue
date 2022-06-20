c3f9c47435be5cf81d29460d6914aace8f2de9ab
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-06-20 13:22:44 +0200

    synlig neste uke

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,20 @@
     >
       <h1>Vaskeliste</h1>
     </nav>
-    <List />
+    <div class="flex flex-col justify-center text-center text-gray-800 py-10">
+      <h1 class="text-3xl pb-10">Uke: {{ weekNumber }}</h1>
+
+      <List :weekNumber="weekNumber" class="text-4xl" />
+
+      <h3 class="pt-20">Ukas bilde</h3>
+      <img
+        :src="`https://picsum.photos/id/${weekNumber}/200/200`"
+        alt="Tilfeldig bilde"
+        class="w-100 pb-20 self-center"
+      />
+      <h2>Neste uke:</h2>
+      <List :weekNumber="weekNumber + 1" />
+    </div>
   </div>
 </template>
 
@@ -14,5 +27,16 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "IndexPage",
+  computed: {
+    weekNumber() {
+      let now = new Date();
+      let onejan = new Date(now.getFullYear(), 0, 1);
+      let weekNumber = Math.ceil(
+        ((now.getTime() - onejan.getTime()) / 86400000 + onejan.getDay() + 1) /
+          7
+      );
+      return weekNumber - 1;
+    },
+  },
 });
 </script>
