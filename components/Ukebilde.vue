afa57dbfdd3ede450e9fc1dbc7964b9ef9e60739
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-06-20 15:03:54 +0200

    bedre bildekvalitet

---
+++ components/Ukebilde.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3 class="pt-20">Ukas bilde</h3>
+    <img
+      :src="`https://picsum.photos/id/${weekNumber}/600/600`"
+      alt="Tilfeldig bilde"
+      class="w-64 pb-20 self-center"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["weekNumber"],
+};
+</script>
