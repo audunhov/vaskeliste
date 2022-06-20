35abf158b88f08f2e17b282c105c2c8caff50cf3
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-06-20 19:41:43 +0200

    link til github

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@
       class="navbar navbar-expand-lg shadow-md p-2 bg-white relative flex items-center w-full justify-between"
     >
       <h1>Vaskeliste</h1>
+      <a href="https://github.com/audunhov/vaskeliste"
+        ><img src="../static/GitHub-Mark-64px.png" class="h-6"
+      /></a>
     </nav>
     <div class="flex flex-col justify-center text-center text-gray-800 py-10">
       <h1 class="text-3xl pb-10">Uke: {{ weekNumber }}</h1>
@@ -17,7 +20,7 @@
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
 import Ukebilde from "~/components/Ukebilde.vue";
 
@@ -32,6 +35,11 @@ export default Vue.extend({
           7
       );
       return weekNumber - 1;
+    },
+    language() {
+      return {
+        language: this.$store.state.language,
+      };
     },
   },
   components: { Ukebilde },
