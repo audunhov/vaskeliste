2f934a28ecd28c3e68bb4f2f504ed0904c2a534b
Audun Hammer Hovda <audun@unicornis.no>
2023-09-27 09:42:08 +0200

    darker background color

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="min-h-screen bg-slate-50 p-5">
+  <div class="min-h-screen bg-gray-100 p-5">
     <div class="max-w-screen-sm mx-auto">
       <slot />
     </div>
