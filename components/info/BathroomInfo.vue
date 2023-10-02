4c5f9ce8c97fe362f22e137fe606fd4aea49d2bc
Audun Hammer Hovda <hammerhovda@gmail.com>
2023-10-02 22:45:15 +0200

    modal support

---
+++ components/info/BathroomInfo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+
+  <ul class="list-disc list-inside">
+    <li>Wash the mirror</li>
+  </ul>
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/info/HallwayInfo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<ul class="list-disc list-inside">
+  <li>Take out glass and metal garbage</li>
+  <li>Take out pant :)</li>
+</ul>
+</template>
+
+<style scoped>
+
+</style>
